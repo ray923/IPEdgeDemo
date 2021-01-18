575c3b9729ac5317076f78787231198237ce2808
Wang Ruiyi <ray923_81@hotmail.com>
2021-01-18 18:18:11 +1100

    sql scriptV0

---
+++ IPedgeProject/sql/data/filldata.sql
@@ -0,0 +1,26 @@
+insert into dbo.Employee values
+(1597,'Russell', 'Crowe',	'2014-01-01 00:00:00',	9597,	1),
+(1598,'Nicole',	'Kidman',	'2014-01-07 00:00:00',	9598,2),
+(1599,'Christian', 'Bale',	'2014-02-28 00:00:00',	9599,3),
+(1600,'Daniel',	'Craig',	'2014-07-31 00:00:00',	NULL,	4),
+(1601,'Gary', 'Oldman',	'2014-09-11 00:00:00',	9601,	5),
+(1602,'Brad', 'Pitt',	'2014-09-13 00:00:00',	9602,	6),
+(1603,'Charlize', 'Théron',	'2015-02-14 00:00:00',	9601,	7),
+(1604,'Jamie', 'Fox',	'2015-03-31 00:00:00',	9604,	8),
+(1605,'Kate', 'Beckinsale',	'2015-12-31 00:00:00',	9605,	9),
+(1606,'Dev', 'Patel',	'2016-02-29 00:00:00',	9606,	10),
+(1607,'Jackie', 'Chang',	'2017-01-19 00:00:00',	8971,	1),
+(1244,'Ray', 'Wang',	'2020-07-03 00:00:00',	11,	1)
+
+insert into dbo.Role values
+('Junior Developer'),
+('Senior Developer'),
+('Project Manager'),
+('Senior Architect'),
+('Product Manager'),
+('Intern'),
+('Managing Director'),
+('UX Designer'),
+('Receptionist'),
+('QA Team Lead'),
+('Scrummaster')
---
+++ IPedgeProject/sql/procedure/pagedEmployee.sql
@@ -0,0 +1,101 @@
+CREATE PROCEDURE [dbo].[Pro_GetPageData]
+
+(
+
+    @TableName        nvarchar(3000),            -- 表名
+
+    @ReturnFields    nvarchar(3000) = '*',    -- 需要返回的列
+
+    @PageSize        int = 10,                -- 每页记录数
+
+    @PageIndex        int = 0,                -- 当前页码
+
+    @Where            nvarchar(3000) = '',        -- 查询条件
+
+    @OrderBy        nvarchar(200),            -- 排序字段名 最好为唯一主键
+
+    @OrderType        int = 1                    -- 排序类型 1:降序 其它为升序
+
+)
+
+AS
+
+    DECLARE @TotalRecord int
+
+    DECLARE @TotalPage int
+
+    DECLARE @CurrentPageSize int
+
+    DECLARE @TotalRecordForPageIndex int
+
+    declare @CountSql nvarchar(4000)
+
+
+
+    if @OrderType = 1
+
+        BEGIN
+
+            set @OrderBy = ' Order by ' + REPLACE(@OrderBy,',',' desc,') + ' desc '
+
+        END
+
+    else
+
+        BEGIN
+
+            set @OrderBy = ' Order by ' + REPLACE(@OrderBy,',',' asc,') + ' asc '
+
+        END
+
+
+
+    -- 总记录
+
+    set @CountSql='SELECT @TotalRecord=Count(*) From '+@TableName+' '+@Where
+
+    execute sp_executesql @CountSql,N'@TotalRecord int out',@TotalRecord out
+
+
+
+    SET @TotalPage=(@TotalRecord-1)/@PageSize+1
+
+
+
+    -- 查询页数不得大于总页数
+
+    if(@PageIndex > @TotalPage)
+
+        set @PageIndex = @TotalPage
+
+
+
+    SET @CurrentPageSize=(@PageIndex-1)*@PageSize
+
+
+
+
+
+    -- 返回记录
+
+    set @TotalRecordForPageIndex=@PageIndex*@PageSize
+
+
+
+    exec    ('SELECT *
+
+            FROM (SELECT TOP '+@TotalRecordForPageIndex+' '+@ReturnFields+', ROW_NUMBER() OVER ('+@OrderBy+') AS ROWNUM
+
+            FROM '+@TableName+ ' ' + @Where +' ) AS TempTable
+
+            WHERE TempTable.ROWNUM >
+
+            '+@CurrentPageSize)
+
+
+
+
+
+    -- 返回总页数和总记录
+
+    SELECT @TotalPage as PageCount,@TotalRecord as RecordCount
